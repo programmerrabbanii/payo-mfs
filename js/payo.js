1162a1a2df63c1c1d3f9cc68f5cd75f671b9126b
programmerrabbanii <rabbanisarkar642@gmail.com>
2024-09-19 11:09:57 +0600

    added home section here

--- js/payo.js
+++ js/payo.js
@@ -1,6 +1,15 @@
-document.getElementById('submit-btn').addEventListener('click', function(){
-    let phoneNumber=document.getElementById('phNumber')
-    let numberOfvalue=phoneNumber.value
-    
+document.getElementById('submit-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    let phoneNumber=document.getElementById('phNumber').value
+    let pinNumber=document.getElementById('pin').value
+
+
+    if(phoneNumber=== '20028' && pinNumber==='1234'){
+        console.log('Thank you , you are logged now');
+        window.location.href='./home.html'
+    } else{
+         alert('opps sorry , wrong phone or pin')
+    }
+
 
 })
