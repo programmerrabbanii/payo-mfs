c518822c31eed1f5387ba38f3af3c493466418a4
programmerrabbanii <rabbanisarkar642@gmail.com>
2024-09-19 10:00:21 +0600

    added the demo style here

---
+++ js/payo.js
@@ -0,0 +1,6 @@
+document.getElementById('submit-btn').addEventListener('click', function(){
+    let phoneNumber=document.getElementById('phNumber')
+    let numberOfvalue=phoneNumber.value
+    
+
+})
